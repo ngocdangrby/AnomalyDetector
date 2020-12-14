2442feff10ba87e9bb18592b6bdc72c6c7df43d4
ngocdangrby <ngocdang8a@gmail.com>
2020-12-14 08:57:44 +0000

    Outlier by MAD

--- src/main/scala/com/main_package/AnomalyApp.scala
+++ src/main/scala/com/main_package/AnomalyApp.scala
@@ -6,14 +6,14 @@ import com.stream_layer.{StartProcessing, StreamOutlierDetectSpark, StreamOutlie
 object AnomalyApp {
   def main(args: Array[String]): Unit = {
     println("Start app")
-    runProcess(args(0), args(1), args(2))
+    runProcess(args(0), args(1), args(2), args(3))
   }
 
-  def runProcess(kafkahost:String, prefixTopic:String, checkpointpath: String): Unit = {
+  def runProcess(kafkahost:String, prefixTopic:String, checkpointpath: String, colName: String): Unit = {
     println("hello")
     val actorSystem = ActorSystem("ActorSystem")
     println("hello2")
-    val streamActor = actorSystem.actorOf(Props(new StreamOutlierProcessActor(new StreamOutlierDetectSpark(kafkahost, prefixTopic, checkpointpath))))
+    val streamActor = actorSystem.actorOf(Props(new StreamOutlierProcessActor(new StreamOutlierDetectSpark(kafkahost, prefixTopic, checkpointpath, colName))))
 
     streamActor ! StartProcessing
   }
--- src/main/scala/com/stream_layer/StreamOutlierDetect.scala
+++ src/main/scala/com/stream_layer/StreamOutlierDetect.scala
@@ -1,4 +1,4 @@
-package com.stream_layer
+package stream_layer
 
 import akka.actor.Actor
 import org.apache.spark.sql.expressions.Window
@@ -6,7 +6,7 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.functions.{abs, col, expr, from_json, from_unixtime, lit, stddev, struct, to_date, to_json, udf, unix_timestamp, window}
 import org.apache.spark.sql.types.{ArrayType, IntegerType, LongType, StringType, StructType, TimestampType}
 
-class StreamOutlierDetectSpark(kafkahost: String, prefixTopic: String, checkpointpath: String){
+class StreamOutlierDetectSpark(kafkahost: String, prefixTopic: String, checkpointpath: String, colName: String ){
 
   val spark = SparkSession.builder().appName("Lambda Architecture - Speed layer")
     .master("local")
@@ -28,127 +28,16 @@ class StreamOutlierDetectSpark(kafkahost: String, prefixTopic: String, checkpoin
 
   val metricCpuDf = df.selectExpr("CAST(value AS STRING)")
 
-  val schema = new  StructType().add("timestamp", TimestampType)
-    .add("pkts", IntegerType).add("interface", StringType)
+  val schema = new  StructType().add("timestamp", LongType)
+    .add(colName, StringType).add("interface", StringType)
 
   val cpuDF = metricCpuDf.select(from_json(col("value"), schema).as("data"))
     .select("data.*")
-  //  def get_median(df: DataFrame, col_name: String) = {
-  //    df.stat.approxQuantile(col_name, Array(0.5), 0.25)
-  //  }
-  //
-  //  def median(inputList: List[Double]): Double = {
-  //    val count = inputList.size
-  //    if (count % 2 == 0) {
-  //      val l = count / 2 - 1
-  //      val r = l + 1
-  //      (inputList(l) + inputList(r)).toDouble / 2
-  //    } else
-  //      inputList(count / 2).toDouble
-  //  }
-  //
-  //  val sub = udf((x: Double, y: Double) => {
-  //    if (x > y) {
-  //      x - y
-  //    } else {
-  //      y - x
-  //    }
-  //  })
-  //
-  //  def cal_mad(df: DataFrame) = {
-  //    var resDf = df
-  //    var median1 = median(resDf.select("usage").collect().map(_ (0).asInstanceOf[Double]).toList)
-  //    //    var med = get_median(df, "usage")
-  //    resDf.withColumn("cal1", col("usage") - median1)
-  //    var median2 = median(resDf.select("cal1").collect().map(_ (0).asInstanceOf[Double]).toList)
-  //    resDf.withColumn("mad", col("cal1") - median2)
-  //    resDf.where("mad >= 3.12")
-  //  }
-
-  //
-
-
-  //  val cal_mad_udf = udf(cal_mad)
-
-  //  var windowedCpuDfCounts = cpuDF.groupBy(
-  //    window(from_unixtime($"timestamp"),
-  //      "2 minutes",
-  //      "1 minutes"),
-  //    $"source")
-
-  //  val windowedCpuDfCounts = cpuDF.select(
-  //    window(from_unixtime($"created_at"),
-  //      "2 minutes",
-  //      "1 minutes"),
-  //    $"source_computer", $"usage", $"created_at")
-  //    .agg(stddev(col("usage")).alias("stddev1"))
-  //    .join(cpuDF,expr("source_computer == source_computer"), "left")
-  //    .agg(stddev(col("stddev1")).alias("mad"))
-  //    .where(col("mad") > 3.1)
-
-
-  //  val test = df.groupby("genre").agg(F.expr("percentile(duration, array(0.5))")[0].alias("duration_median")).join(df, "genre", "left")
-  //    .withColumn("duration_difference_median", F.abs(F.col("duration") - F.col("duration_median") ) ).groupby("genre", "duration_median")
-  //    .agg(F.expr("percentile(duration_difference_median, array(0.5))")[0].alias("median_absolute_difference"))
-
-  //  val withStdDF = windowedCpuDfCounts.select(
-  //     $"stddev1"
-  //  ).agg(stddev(col("sttdev1")).alias("mad"))
-  //    .where($"mad" > 3.0)
-  //  var test = cpuDF.groupBy(
-  //    window(from_unixtime($"created_at"),
-  //      "2 minutes",
-  //      "1 minutes"),
-  //    col("source_computer").alias("source_computer1")
-  //  ).agg(expr("percentile(usage, array(0.5))")(0).alias("usage_median"))
-  //
-  //  val writeMem = test.writeStream
-  //    .format("memory")
-  //    .queryName("firstMedianDF")
-  //    .outputMode("complete")
-  //    .start()
-  //
-  //  val firstDF = spark.sql("select * from firstMedianDF")
-  //
-  //  var madDF = firstDF.join(cpuDF, expr("source_computer1==source_computer"), "inner")
-  //    .withColumn("usage_diff_median", abs(col("usage") - col("usage_median")))
-  //    .groupBy("source_computer", "usage_median")
-  //    .agg(expr("percentile(usage_diff_median, array(0.5))")(0).alias("mad"))
-  //
-  //  val res = cpuDF.withWatermark("created_at", "10 minutes").select(col("source_computer").alias("sc"), col("usage"))
-  //    .join(madDF, expr("sc==source_computer"), "inner").filter(abs(col("usage") - col("usage_median")) >= col("mad") * 3)
-  //
-  //  val writeConsole = res.writeStream
-  //    .format("console")
-  //    .queryName("anomaly")
-  //    .outputMode("append")
-  //    .start()
-  //
-  //  writeMem.awaitTermination()
-  //  writeConsole.awaitTermination()
-  //   val test = cpuDF.groupBy(window(from_unixtime($"created_at"),
-  //          "2 minutes",
-  //          "1 minutes"), col("usage")).withColumn("usage_median", expr("percentile(usage, array(0.5))")(0))
-  //    .withColumn("diff", abs(col("usage_median") - col("usage")))
-
-  //  val cpudf2 = cpuDF.withColumn("created_at",from_unixtime($"created_at" ))\
-
-  //    val q1 = test.writeStream
-  //      .format("memory")
-  //      .queryName("testDF")
-  //      .start()
-  //  val testsql = spark.sql("select * from testDF")
-  //    val test2 = testsql.withColumn("diff",abs(col("median") - col("usage")))
-  //    val test3 = test2.withWatermark("created_at", "10 minutes")
-
-
-  //    val res = test.withWatermark("created_at", "10 minutes")
-  //      .groupBy(window(col("created_at"), "5 minutes")).count()
-  //  val test2 = test.withWatermark("created_at","10 minutes")
-
-  cpuDF.withColumn("created_at",unix_timestamp(col("timestamp")/1000))
-  val test = cpuDF.withWatermark("created_at", "20 seconds")
-    .select(expr("percentile(usage, array(0.5))").alias("median"))
+    
+  val cpuDfs = cpuDF.withColumn("timestamp",(col("timestamp")/1000).cast("timestamp"))
+  val cpuDFnew = cpuDfs.withColumn(colName, col(colName).cast(LongType))
+  val test = cpuDFnew.withWatermark("timestamp", "5 minutes")
+    .select(expr("percentile(" +colName +", array(0.5))").alias("median"))
   //    var median1 = test.select(col("median")).first().getDouble(0)
   val q1 = test.writeStream
     .queryName("medianTable")
@@ -156,8 +45,8 @@ class StreamOutlierDetectSpark(kafkahost: String, prefixTopic: String, checkpoin
     .outputMode("complete")
     .start()
   val median1 = spark.sql("select median from medianTable")
-  val cpuDF2 = cpuDF.crossJoin(median1)
-  val cpuDF3 = cpuDF2.withColumn("diff1", abs(col("usage") - col("median")(0)))
+  val cpuDF2 = cpuDFnew.crossJoin(median1)
+  val cpuDF3 = cpuDF2.withColumn("diff1", abs(col(colName) - col("median")(0)))
   val med2Df = cpuDF3
     .select(expr("percentile(diff1, array(0.5))").alias("median2"))
 
@@ -169,23 +58,23 @@ class StreamOutlierDetectSpark(kafkahost: String, prefixTopic: String, checkpoin
 
   val median2 = spark.sql("select median2 from median2Table")
   val cpuDF4 = cpuDF3.crossJoin(median2)
-  val cpuDF5 = cpuDF4.withColumn("mad", abs((col("usage") - col("median")(0))/(col("median2")(0)*1.4826)))
-  val cpuDF6 = cpuDF5.where(col("mad") >= 3.2)
+  val cpuDF5 = cpuDF4.withColumn("mad", abs((col(colName) - col("median")(0))/(col("median2")(0)*1.4826)))
+  val cpuDF6 = cpuDF5.where(col("mad") >= 3.12)
 
-//  val writeConsole = cpuDF6.writeStream
-//    .format("console")
-//    .queryName("anomaly")
-//    .outputMode("append")
-//    .start()
+  //  val writeConsole = cpuDF6.writeStream
+  //    .format("console")
+  //    .queryName("anomaly")
+  //    .outputMode("append")
+  //    .start()
   cpuDF6.printSchema()
-val writeConsole = cpuDF6.select(to_json(struct($"created_at",$"usage",$"source_computer")).alias("value"))
-  .selectExpr( "CAST(value AS STRING)")
-  .writeStream
-  .format("kafka")
-  .option("kafka.bootstrap.servers", kafkahost)
-  .option("topic", "alert")
-  .option("checkpointLocation", checkpointpath)
-  .start()
+  val writeConsole = cpuDF6.select(to_json(struct($"timestamp",col(colName),$"interface")).alias("value"))
+    .selectExpr( "CAST(value AS STRING)")
+    .writeStream
+    .format("kafka")
+    .option("kafka.bootstrap.servers", kafkahost)
+    .option("topic", "alert")
+    .option("checkpointLocation", checkpointpath)
+    .start()
 
   q1.awaitTermination()
   q2.awaitTermination()
